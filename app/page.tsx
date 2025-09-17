08abcc39040951445d85e485bb8e997ac9127aaa
EternalKnight002 <resoamankumar@gmail.com>
2025-09-18 02:15:59 +0530

    vercel-speed-insight

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import SkillsSection from '../components/SkillsSection'
 import ProjectsSection from '../components/ProjectsSection'
 import ExploringSection from '../components/ExploringSection'
 import ContactSection from '../components/ContactSection'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
